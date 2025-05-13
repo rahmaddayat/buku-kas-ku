5dbb26405e15c2d3f6add80280ae744cc01e9673
RahmadHidayatullah <166033080+Ryonn12@users.noreply.github.com>
2025-05-13 20:03:22 +0700

    feat:add dashboard

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,13 +1,21 @@
-// app/(pages)/main/page.tsx
+import Navbar from "@/components/navbar/navbar";
+import Card from "@/components/card/card";
+import Header from "@/components/header/header";
 
 export default function MainPage() {
   return (
-    <main className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Halaman Main</h1>
-      <p className="text-gray-700">
-        Ini adalah halaman <strong>Main</strong> dari aplikasi Next.js dengan
-        App Router.
-      </p>
-    </main>
+    <div className="flex">
+      <Navbar />
+      <div className="flex flex-col ml-64 w-full min-h-screen bg-gray-100">
+        <Header title="Finance Dashboard" />
+        <main className="p-8 flex-1">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+            <Card title="Total Balance" description="Rp0.00" />
+            <Card title="Income" description="Rp0.00" />
+            <Card title="Expenses" description="Rp0.00" />
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
