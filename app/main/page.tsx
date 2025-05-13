4c218d357dfdec81f268acc6284c9cb8fc8af45e
RahmadHidayatullah <166033080+Ryonn12@users.noreply.github.com>
2025-05-13 18:26:02 +0700

    update folder

---
+++ app/main/page.tsx
@@ -0,0 +1,13 @@
+// app/(pages)/main/page.tsx
+
+export default function MainPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Halaman Main</h1>
+      <p className="text-gray-700">
+        Ini adalah halaman <strong>Main</strong> dari aplikasi Next.js dengan
+        App Router.
+      </p>
+    </main>
+  );
+}
